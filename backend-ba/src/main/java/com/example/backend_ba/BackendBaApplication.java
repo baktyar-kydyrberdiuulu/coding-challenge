a15c47a17eb3d823ab150b9e0bcbac59431ae8dc
kydyrberdi <baktyiar.kydyrberdi@navimatix.de>
2025-02-26 09:11:19 +0100

    initial commit

---
+++ backend-ba/src/main/java/com/example/backend_ba/BackendBaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_ba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBaApplication.class, args);
+	}
+
+}
