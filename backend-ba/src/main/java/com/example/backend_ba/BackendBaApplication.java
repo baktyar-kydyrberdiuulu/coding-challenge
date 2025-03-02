8bbb9e823350bcfe6a2443abdb5c8e8b0c2bc66a
kydyrberdi <baktyiar.kydyrberdi@navimatix.de>
2025-03-02 11:38:52 +0100

    Quick Sorting/ Learning by doing. 02/60

--- backend-ba/src/main/java/com/example/backend_ba/BackendBaApplication.java
+++ backend-ba/src/main/java/com/example/backend_ba/BackendBaApplication.java
@@ -2,6 +2,7 @@ package com.example.backend_ba;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -13,8 +14,13 @@ public class BackendBaApplication {
 		int[] bubleUnsortet = {12,3,45,44,32,-123,54,33};
 		Algorithm algorithm = new Algorithm();
 		algorithm.bubbleSort(bubleUnsortet);
+		int[] quicksortListe = {12,4,3,11,25,89,22,123};
+		System.out.println("Vor dem Sortieren: " + Arrays.toString(quicksortListe));
+		algorithm.sort(quicksortListe);
 
-		System.out.println("Der größte Wert in der Liste ist: "+ algorithm.ermittleMaximum(numberList));
+		System.out.println("Nach dem Sortieren: " + Arrays.toString(quicksortListe));
+
+//		System.out.println("Der größte Wert in der Liste ist: "algorithm.sort(quicksortListe));
 	}
 
 }
