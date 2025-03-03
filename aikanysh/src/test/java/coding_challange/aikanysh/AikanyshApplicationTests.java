71e43eb18cf1a668ee56f1ca5d0fc4341b0d5a05
Aikanysh <ryskulovaaikanysh@gmail.com>
2025-03-03 23:10:58 +0100

    Bubble Sort für Strings

---
+++ aikanysh/src/test/java/coding_challange/aikanysh/AikanyshApplicationTests.java
@@ -0,0 +1,13 @@
+package coding_challange.aikanysh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikanyshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
